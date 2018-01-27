5913f7ae24e7d778d635bc482fd9262764febe80
hell2o <jes566@york.ac.uk>
2018-01-27 16:23:14 +0000

    Added a number of object prefabs

---
+++ CreepyHouse/Assets/Scripts/Audio/PingTestLoop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingTestLoop : MonoBehaviour {
+	public float speed;
+	public float falloff;
+	public float width;
+	public float delay;
+	float timeSinceEmit = 0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeSinceEmit += Time.deltaTime;
+		if (timeSinceEmit > delay) {
+			timeSinceEmit -= delay;
+			EmitManager.Instance.Emit (this.transform.position, speed, falloff, width);
+		}
+	}
+}
