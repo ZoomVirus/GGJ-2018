072897b1f72027cd8d9d1fe223b6b217acce5cd3
mattthehoopy <matt.gough102@gmail.com>
2018-01-27 09:25:20 +0000

    Created Menus and Scene Layouts
    Added Game Scene, basic menus, button logic, level loader and fading manager for nice transitions

--- CreepyHouse/Assets/Scripts/Tech/Fader.cs
+++ CreepyHouse/Assets/Scripts/Tech/Fader.cs
@@ -1,16 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class Fader : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private float m_fadeTime = 0.3f;
+    Image m_image;
+    bool m_fading = false;
+
+    private void Start()
+    {
+        m_image = GetComponent<Image>();
+        FadeIn();
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(Fade(true));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(Fade(false));
+    }
+
+    public bool Fading() { return m_fading; }
+
+    IEnumerator Fade(bool fadeIn)
+    {
+        if (m_fading)
+        {
+            yield break;
+        }
+        m_fading = true;
+
+        float time = m_fadeTime;
+        Color col = m_image.color;
+
+        while((time -= Time.deltaTime) > 0)
+        {
+            float s = (time / m_fadeTime);
+
+            s = fadeIn ? s : 1 - s; 
+
+            col.a = s;
+            m_image.color = col;
+
+            yield return null;
+        }
+
+        col.a = fadeIn ? 0 : 1;
+        m_image.color = col;
+        m_fading = false;
+
+    }
+
+    //public void ShakeItAllAbout()
+    //{
+    //}
 }
