350c8b278e0c1c2895ac97fea14499b586a4dafa
mattthehoopy <matt.gough102@gmail.com>
2018-01-27 07:36:53 +0000

    Stub Fader Code Added.

---
+++ CreepyHouse/Assets/Scripts/Tech/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour {
+
+    bool m_levelLoadRequested = false;
+
+    public void LoadLevel(string level)
+    {
+        if (!m_levelLoadRequested)
+        {
+            StartCoroutine(LoadLevelCoroutine(level));
+        }
+    }
+
+    IEnumerator LoadLevelCoroutine(string level)
+    {
+        
+        yield return null;
+    }
+
+}
