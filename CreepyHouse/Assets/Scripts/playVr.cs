9381bae4e43da2f3289aadc2c98d1074f13ccd6f
ZoomVirus <33657836+ZoomVirus@users.noreply.github.com>
2018-01-28 12:57:09 +0000

    menus support in vr and title update

---
+++ CreepyHouse/Assets/Scripts/playVr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playVr : MonoBehaviour
+{
+
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        GlobalSettings.RiftContoller = true;
+    }
+}
