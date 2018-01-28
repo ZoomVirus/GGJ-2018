090538b287b0e0bc5080d407104883e1d3122750
ZoomVirus <33657836+ZoomVirus@users.noreply.github.com>
2018-01-28 14:06:59 +0000

    rift update

--- CreepyHouse/Assets/Scripts/Controller/GlobalSettings.cs
+++ CreepyHouse/Assets/Scripts/Controller/GlobalSettings.cs
@@ -8,11 +8,11 @@ public class GlobalSettings : MonoBehaviour
     public static float rotateHoriSpeed = 30.0f;
     public static float rotateVertSpeed = -10.0f;
     public static bool XboxContoller = false;
-    public static bool RiftContoller = true;
+    public static bool RiftContoller = false;
     public static float xboxControllerToKeyboardRatioTranslation = 10;
     public static float xboxControllerToMouseRatioRotation = 20;
-    public static float riftControllerToKeyboardRatioTranslation = 0.1f;
-    public static float riftControllerToMouseRatioTranslation = 3f;
+    public static float riftControllerToKeyboardRatioTranslation = 0.05f;
+    public static float riftControllerToMouseRatioTranslation = 2f;
     public static float ForceThrowObject = 2;
     public static bool AllowedToMove = true;
     // Use this for initialization
